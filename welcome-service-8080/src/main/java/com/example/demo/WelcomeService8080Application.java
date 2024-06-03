18adacfc04ff35d98267e85bcb1c7fde855030d1
大熊熊 <s86390474@yahoo.com>
2024-06-04 06:15:09 +0800

    welcome

---
+++ welcome-service-8080/src/main/java/com/example/demo/WelcomeService8080Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeService8080Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeService8080Application.class, args);
+	}
+
+}
