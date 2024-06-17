ae32d2bcb352456cc8b4cdcdb36d8817b49c2f34
大熊熊 <s86390474@yahoo.com>
2024-06-18 07:02:45 +0800

    welcome

---
+++ feign-customer-service-9092/src/main/java/com/example/demo/FeignCustomerService9092Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignCustomerService9092Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignCustomerService9092Application.class, args);
+	}
+
+}
---
+++ feign-product-service-9091/src/test/java/com/example/demo/FeignProductService9091ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignProductService9091ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
