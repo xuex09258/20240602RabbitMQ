d5b536d36a798e6ea62fb6f3cfe5a4767d675951
大熊熊 <s86390474@yahoo.com>
2024-06-24 07:18:19 +0800

    load

---
+++ loadbalancer-service-b-8081/src/main/java/com/example/demo/LoadbalancerServiceB8081Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadbalancerServiceB8081Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadbalancerServiceB8081Application.class, args);
+	}
+
+}
