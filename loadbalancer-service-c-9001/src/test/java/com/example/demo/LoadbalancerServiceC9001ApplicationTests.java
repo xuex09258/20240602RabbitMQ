8502f842b7cd2d3cbb4135bb6d69b702a208d717
大熊熊 <s86390474@yahoo.com>
2024-06-24 07:23:16 +0800

    load1

---
+++ loadbalancer-service-c-9001/src/test/java/com/example/demo/LoadbalancerServiceC9001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbalancerServiceC9001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
