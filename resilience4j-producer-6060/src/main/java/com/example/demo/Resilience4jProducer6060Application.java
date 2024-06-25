9d9a1f41249a8b1d19e3146adbc499cf58e0b7c2
大熊熊 <s86390474@yahoo.com>
2024-06-26 07:28:43 +0800

    s

---
+++ resilience4j-producer-6060/src/main/java/com/example/demo/Resilience4jProducer6060Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jProducer6060Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jProducer6060Application.class, args);
+	}
+
+}
